89f301cc3dbb76fddd18b3b80a843e24b1c4c7c9
황승민 <84918205+sxunxin@users.noreply.github.com>
2024-03-25 21:02:31 +0900

    Add files via upload

---
+++ SeungMin/황승민:1주차 과제/Q06.c
@@ -0,0 +1,17 @@
+
+// 6. 국어, 영어, 수학 점수를 저장할 변수(kor, eng, mat)를 선언하고 각각 70, 80, 90점으로 초기화합니다.
+// 총점을 저장할 변수(tot)를 선언하여 세 과목의 합을 구하고 세 과목의 점수와 총점을 출력하는 프로그램을 작성하세요.
+
+#include <stdio.h>
+
+int main(void) {
+
+    int kor=70, eng=80, mat=90, tot = 0;
+
+    tot = kor + eng + mat;
+
+    printf("국어 : %d\n영어 : %d\n수학 : %d\n총점 : %d\n", kor, eng, mat, tot);
+
+
+    return 0;
+}
