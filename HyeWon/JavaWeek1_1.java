9603a64a76c48d4519c61f765e565ddfb6dbabdf
오혜원 <163323378+hyewon2da@users.noreply.github.com>
2024-06-28 02:58:33 +0900

    Add files via upload

---
+++ HyeWon/JavaWeek1_1.java
@@ -0,0 +1,14 @@
+package java_1;
+
+public class JavaWeek1_1
+ {
+
+    public static void main(String[] args)
+    {
+        int num=30;
+        String name="자바";
+        System.out.println("당신의 이름을 입력하세요:"+ name );
+        System.out.println("당신의 나이를 입력하세요:"+ num);
+        System.out.println("당신의 이름은"+name+"이고, 나이는 "+num+"입니다.");
+    }
+}
